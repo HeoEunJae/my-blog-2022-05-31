0a9c7b1441afc5ef4a82ff2c104c23d89d76e36f
HeoEunJae <dmswo159@gmail.com>
2022-05-31 12:49:21 +0900

    테스트페이지

---
+++ src/pages/test.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// styles
+
+// markup
+const test = () => {
+  return (
+    <main>
+      <title>테스트 페이지</title>
+      <h1>테스트 페이지 입니다/.</h1>
+    </main>
+  );
+};
+
+export default test;
