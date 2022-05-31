8247f30c7006af48d53714f20d7462b9447cc063
HeoEunJae <dmswo159@gmail.com>
2022-05-31 12:50:56 +0900

    테스트 스페이스 페이지

---
+++ src/pages/test-space.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// styles
+
+// markup
+const test_space = () => {
+  return (
+    <main>
+      <title>테스트 스페이스 페이지</title>
+      <h1>테스트 스페이스 페이지 입니다/.</h1>
+    </main>
+  );
+};
+
+export default test_space;
